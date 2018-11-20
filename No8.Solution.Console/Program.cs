e59e9c193a1d382ebdd5e007b7c0feb8180a1cd8
HannaZhuravskaya <hanna.zhuravskaya@gmail.com>
2018-11-20 18:50:25 +0300

    Read printers from App.config. Added errors to NLog.config. Fixed bugs.

--- No8.Solution.Console/Program.cs
+++ No8.Solution.Console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Windows.Forms;
 using NLog;
@@ -11,57 +12,51 @@ namespace No8.Solution.Console
 {
     class Program
     {
+        private static NLog.Logger _logger;
+        
         [STAThread]
         static void Main(string[] args)
         {
-            var printerManager = new PrinterManager();
-            var printerNames = new List<string>() { EpsonPrinter.Name, CanonPrinter.Name };
-
-            printerManager.OnIsBeingPrinted += Logger;
-            printerManager.OnPrinted += Logger;
-
-
-            System.ConsoleKeyInfo key;
-            do
+            try
             {
-                System.Console.WriteLine("\nSelect your choice:");
-                System.Console.WriteLine("1:Add new printer");
-                System.Console.WriteLine("2:Choose printer and print");
-                System.Console.WriteLine("3:Exit");
+                
+                CreateLogger();
+                GetNamesOfExistingPrinters();
+                
+                var printerManager = new PrinterManager();
+                var printerNames = GetNamesOfExistingPrinters();
 
-                key = System.Console.ReadKey();
+                printerManager.OnCurrentPrint += LogPrinterManager;
 
-                if (key.Key == ConsoleKey.D1)
+                System.ConsoleKeyInfo key;
+                do
                 {
-                    CreatePrinter(printerManager, printerNames);
-                }
+                    System.Console.WriteLine("\nSelect your choice:");
+                    System.Console.WriteLine("1:Add new printer");
+                    System.Console.WriteLine("2:Choose printer and print");
+                    System.Console.WriteLine("3:Exit");
 
-                if (key.Key == ConsoleKey.D2)
-                {
-                    Print(printerManager, printerNames);
-                }
+                    key = System.Console.ReadKey();
 
+                    if (key.Key == ConsoleKey.D1)
+                    {
+                        CreatePrinter(printerManager, printerNames);
+                    }
 
-            } while (key.Key != ConsoleKey.D3);
+                    if (key.Key == ConsoleKey.D2)
+                    {
+                        ChooseAndPrint(printerManager, printerNames);
+                    }
+                } while (key.Key != ConsoleKey.D3);
+            }
+            catch (Exception e)
+            {
+                _logger.Error(e);
+                throw;
+            }
         }
 
-        public static void Logger(object o, PrinterEventArgs args)
-        {
-            var config = new NLog.Config.LoggingConfiguration();
-
-            var logfile = new NLog.Targets.FileTarget("logfile") { FileName = "logfile.txt" };
-            
-            config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
-
-            NLog.LogManager.Configuration = config;
-
-            var logger = NLog.LogManager.GetCurrentClassLogger();
-
-            logger.Info($"Info: {args.Info}\n{args.DocumentPrintFilingTime}: {args.PrinterName} - {args.PrinterModel}");
-            logger.Debug($"{args.DocumentPrintFilingTime}: {args.PrinterName} - {args.PrinterModel}");
-        }
-
-        private static void Print(PrinterManager manager, List<string> printerNames)
+        private static void ChooseAndPrint(PrinterManager manager, List<string> printerNames)
         {
             var chosenPrinter = ChoosePrinter(manager, printerNames);
             if (chosenPrinter == null)
@@ -81,73 +76,105 @@ namespace No8.Solution.Console
                     manager.Print(chosenPrinter, stream);
                 } 
             } 
-           // PrinterManager.Log("Printed on Epson");
-        }
-
-        private static Printer ChoosePrinter(PrinterManager manager, List<string> printerNames)
-        {
-            System.Console.WriteLine("\nChoose printer name:");
-            for (int i = 0; i < printerNames.Count; ++i)
-            {
-                System.Console.WriteLine($"{i+1}. " + printerNames[i]);
-            }
-
-            int.TryParse(System.Console.ReadLine(), out var nameIndex);
-            if (nameIndex > 0 && nameIndex < printerNames.Count + 1)
-            {
-                var printers = new List<Printer>(manager.GetPrinters(printerNames[nameIndex - 1]));
-
-                if (printers.Count == 0)
-                {
-                    return null;
-                }
-
-                for (int i = 0; i < printers.Count; ++i)
-                {
-                    System.Console.WriteLine($"{i + 1}. {printers[i]}");
-                }
-               
-                int.TryParse(System.Console.ReadLine(), out var printerIndex);
-                if (printerIndex > 0 && printerIndex < printers.Count + 1)
-                {
-                    return printers[printerIndex - 1];
-                }
-            }
-
-            return null;
         }
 
         private static void CreatePrinter(PrinterManager manager, List<string> printerNames)
         {
-            System.Console.WriteLine("\nChoose printer name:");
-            for (int i = 0; i < printerNames.Count; ++i)
+            int nameIndex = ChoosePrinter(printerNames);
+
+            System.Console.WriteLine("Enter printer model");
+            var model = System.Console.ReadLine();
+
+            var printer = CreateChosenPrinter(printerNames[nameIndex], model);
+
+            if (printer != null)
             {
-                System.Console.WriteLine($"{i + 1}. " + printerNames[i]);
-            }
-
-            int.TryParse(System.Console.ReadLine(), out var nameIndex);
-            if (nameIndex > 0 && nameIndex < printerNames.Count + 1)
-            {
-                System.Console.WriteLine("Enter printer model");
-                var model = System.Console.ReadLine();
-                Printer printer = null;
-                switch (nameIndex - 1)
-                {
-                    case 0:
-                        printer = new EpsonPrinter(model);
-                        break;
-                    case 1:
-                        printer = new CanonPrinter(model);
-                        break;     
-                }
-
-                if (printer != null)
-                {
-                    manager.AddPrinter(printer);
-                }
-
+                manager.AddPrinter(printer);
                 System.Console.WriteLine("\nPrinter added");
             }
+        }
+
+        private static List<string> GetNamesOfExistingPrinters()
+        {
+            var printerNames = new List<string>();
+
+            int count = 1;
+            string currentPrinter = System.Configuration.ConfigurationManager.AppSettings["printer" + count];
+            while (currentPrinter != null)
+            {
+                printerNames.Add(currentPrinter);
+                count++;
+                currentPrinter = System.Configuration.ConfigurationManager.AppSettings["printer" + count];
+            }
+
+            return printerNames;
+        }
+
+        private static Printer ChoosePrinter(PrinterManager manager, List<string> printerNames)
+        {
+            var nameIndex = ChoosePrinter(printerNames);
+
+            var printers = new List<Printer>(manager.GetPrinters(printerNames[nameIndex]));
+
+            if (printers.Count == 0)
+            {
+                return null;
+            }
+
+            var printerIndex = ChoosePrinter(printers);
+
+            return printers[printerIndex];
+        }
+
+        private static int ChoosePrinter<T>(List<T> printerParameters)
+        {
+            int nameIndex;
+            do
+            {
+                System.Console.WriteLine("\nChoose printer name:");
+                for (int i = 0; i < printerParameters.Count; ++i)
+                {
+                    System.Console.WriteLine($"{i + 1}. " + printerParameters[i]);
+                }
+            } while (!int.TryParse(System.Console.ReadLine(), out nameIndex) || nameIndex <= 0 ||
+                     nameIndex > printerParameters.Count);
+
+            return nameIndex - 1;
+        }
+
+        private static Printer CreateChosenPrinter(string printerName, string model)
+        {
+            Printer printer = null;
+            switch (printerName.ToUpper(CultureInfo.CurrentCulture))
+            {
+                case "EPSON":
+                    printer = new EpsonPrinter(model);
+                    break;
+                case "CANON":
+                    printer = new CanonPrinter(model);
+                    break;
+            }
+
+            return printer;
+        }
+
+        public static void LogPrinterManager(object o, PrinterEventArgs args)
+        {
+            _logger.Info($"Info: {args.Info}\n{args.DocumentPrintFilingTime}: {args.PrinterName} - {args.PrinterModel}");
+            _logger.Debug($"{args.DocumentPrintFilingTime}: {args.PrinterName} - {args.PrinterModel}");
+        }
+
+        private static void CreateLogger()
+        {
+            var config = new NLog.Config.LoggingConfiguration();
+
+            var logfile = new NLog.Targets.FileTarget("logfile") { FileName = "logfile.txt" };
+
+            config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
+
+            NLog.LogManager.Configuration = config;
+
+            _logger = NLog.LogManager.GetCurrentClassLogger();
         }
     }
 }
